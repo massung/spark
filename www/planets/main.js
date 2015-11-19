0981254b83eb55ab8e74192111ba460887e42f8e
Jeff Thompson <mythrndr@gmail.com>
2015-11-18 18:19:42 -0600

    Have camera viewport zooming at the center of the map

--- www/planets/main.js
+++ www/planets/main.js
@@ -1,10 +1,16 @@
 var main = spark.main('canvas', 1280, 768);
 
+var viewportScale = 0.75;
+var viewportScaleRate = 0.5;
+
 // Called once the spark framework is fully loaded.
 main.init = function () {
 	spark.game.run('planets/project.json', (function(scene) {
 		// Change the projection so the origin is in the middle.
-    scene.setProjection('middle', 1);
+    scene.setPlayfield('middle', spark.view.canvas.width * 2, spark.view.canvas.height * 2);
+
+
+    scene.setViewport(scene.width * viewportScale, scene.height * viewportScale);
 
 		this.bgLayer = scene.addLayer(new spark.layer.BackgroundLayer());
 		this.planetLayer = scene.addLayer(new spark.layer.SpriteLayer(10));
@@ -12,7 +18,7 @@ main.init = function () {
 
 		// Setup the background.
     this.bgLayer.image = spark.project.assets.starfield;
-    this.bgLayer.m.setScale(2);
+    this.bgLayer.m.setScale(6, 4);
 
 		this.createPlayer(this.playerLayer);
 
@@ -33,20 +39,33 @@ main.createPlayer = function(layer) {
 main.createPlanet = function(num, layer, x, y, scale) {
 	var sprite = layer.spawn();
 
-	sprite.setImage(spark.project.assets["planet_" + num]);
+	sprite.image = spark.project.assets["planet_" + num];
 	sprite.m.setTranslation(
 		x || spark.util.rand(spark.game.scene.left, spark.game.scene.right),
     y || spark.util.rand(spark.game.scene.top, spark.game.scene.bottom));
 
-	sprite.m.setScale(scale || 0.20);
+	sprite.m.setScale(scale || .1);
 
 	return sprite;
 }
 
 // Handle player input.
 main.playerControls = function() {
-	if (spark.input.keyDown(spark.input.KEY.W))
-    spark.game.scene.camera.scale(2.0 * spark.game.step);
-  if (spark.input.keyDown(spark.input.KEY.S))
-    spark.game.scene.camera.scale(-2.0 * spark.game.step);
+	if (spark.input.keyDown(spark.input.KEY.W)) {
+		setViewportScale(viewportScaleRate * spark.game.step);
+		spark.game.scene.setViewport(spark.game.scene.width * viewportScale, spark.game.scene.height * viewportScale);
+	}
+  if (spark.input.keyDown(spark.input.KEY.S)) {
+		setViewportScale(-viewportScaleRate * spark.game.step);
+		spark.game.scene.setViewport(spark.game.scene.width * viewportScale, spark.game.scene.height * viewportScale);
+	}
+}
+
+var setViewportScale = function(adjustment) {
+	viewportScale += adjustment;
+	if (viewportScale < 0.1) {
+		viewportScale = 0.1;
+	} else if (viewportScale > 1.0) {
+		viewportScale = 1.0;
+	}
 }
